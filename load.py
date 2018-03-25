5285e102a278e836745bcbec69e103fd2f7d8120
Andy Mell <afmell@afmell.com>
2018-03-25 21:42:34 +0100

    Fixing various typos, removing unused imports, making reports work by using valid ship names for Thargoids

--- load.py
+++ load.py
@@ -2,27 +2,27 @@
 
 import os
 import Tkinter as tk
-import myNotebook as nb
-from config import config
 import sys
-import plug
+import threading
 import requests
-from sys import platform
 
 this = sys.modules[__name__]	# For holding module globals
 
-bonds = { "marauder": 10000, "cyclops": 2000000, "basilisk": 6000000, "medusa": 10000000 }
+this.kill_count = 0
+this.bond_count = 0
 
-def local_file(name):
-    return os.path.join(os.path.dirname(os.path.realpath(__file__)), name)
+bonds = { "Scout": 10000, "Cyclops": 2000000, "Basilisk": 6000000, "Medusa": 10000000 }
+
+
 
 def plugin_start():
     #this._IMG_CLIPBOARD = tk.PhotoImage(file = local_file('clipboard.gif'))
     return "FireDamage"
 
 def plugin_stop():
+    pass
 
-def _plugin_app(parent):
+def plugin_app(parent):
     """
     Returns a frame containing the status fields we want to display to the 
     app in the main window
@@ -31,14 +31,14 @@ def _plugin_app(parent):
  
     # Current Action being recommended 
     this.status = tk.StringVar() 
-    tk.Label(this.status_frame, textvariable=this.action).grid(row=0, column = 0,sticky=tk.W)
+    tk.Label(this.status_frame, textvariable=this.status).grid(row=0, column = 0,sticky=tk.W)
 
     return this.status_frame
 
 def matches(d, field, value):
-   return field in d and value == d[field]:
+   return field in d and value == d[field]
 
-class Reporter(thraeding.Thread):
+class Reporter(threading.Thread):
     def __init__(self, payload):
         threading.Thread.__init__(self)
         self.payload = payload.copy()
@@ -46,8 +46,9 @@ class Reporter(thraeding.Thread):
     def run(self):
         try:
             requests.post("https://docs.google.com/forms/d/e/1FAIpQLSdP0eoGf2Cq8uEze6ujmdmZGOiRZ2m2sU7eJe4lXEJzPLj32w/formResponse", data = self.payload)
+            print("[fire-damage] After post")
         except:
-            print("[fire-damage] Issue posting message " + str(sys.exc_info()[0])
+            print("[fire-damage] Issue posting message " + str(sys.exc_info()[0]))
       
 def report(**fields):
     
@@ -57,11 +58,16 @@ def report(**fields):
         payload['entry.132486708'] = fields['system']
     if 'ship' in fields:
         payload['entry.1003893312'] = fields['ship']
+        print('[fire-damage] ship : ' + fields['ship'])
     if 'ship_count' in fields: # Confirmed
         payload['entry.1351220981'] = fields['ship_count']
+        this.kill_count = this.kill_count + fields['ship_count']
     if 'bonds' in fields:
-        payload['entry.1086192416'] = fields['reward']
+        payload['entry.1086192416'] = fields['bonds']
+        this.bond_count = this.bond_count + fields['bonds']
+    this.status.set("{} kills, {} CR bonds redeemed this session".format(this.kill_count, this.bond_count))
     payload['submit'] = 'Submit'
+    print("[fire-damage] Reporting a Thargoid state {}".format(payload))
     # Async update to avoid GUI jamming
     Reporter(payload).start()
 
@@ -76,16 +82,18 @@ def reportKill(cmdr, system, reward):
     return report( system = system, ship = rewardToShip(reward), ship_count = 1 )
 
 def reportBond(cmdr, system, reward):
-    return report( system = system, reward = reward )
+    return report( system = system, bonds = reward )
 
 def journal_entry(cmdr, is_beta, system, station, entry, state):
     if is_beta:
         return
     if entry['event'] == "FactionKillBond":
-        if matches(entry, 'VictimFaction', '$faction_Thargoid') and 'Reward' in entry:
-            reportKill(cmdr, system, entry['Reward')
+        print("[fire-damage] Bond")
+        if matches(entry, 'VictimFaction', '$faction_Thargoid;') and 'Reward' in entry:
+            reportKill(cmdr, system, entry['Reward'])
 
     if entry['event'] == 'RedeemVoucher':
+        print("[fire-damage] RedeemBond")
         if matches(entry, 'Faction', 'PilotsFederation') and matches(entry, 'Type', 'CombatBond'):
             
             reportBond(cmdr, system, entry['Amount'])
